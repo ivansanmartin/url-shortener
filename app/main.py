92f4df42bda00e41be7c6bb60618796db2080a83
ivansanmartin <ivansanmartin987@gmail.com>
2025-03-14 23:29:14 -0300

    test cors

--- app/main.py
+++ app/main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI, Depends, status, HTTPException
 from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.v1.endpoints.url_shortener import router as url_shortener_router
 from app.services.url_shortener import UrlShortenerService
 from app.core.dependencies import get_url_shortener_service
 
 app = FastAPI()
-    
+
+origins = [
+    "https://ivsm.link",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(url_shortener_router, prefix="/api/v1")
 
 @app.get("/{slug}", status_code=status.HTTP_200_OK)
